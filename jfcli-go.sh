ae514f60554f6c306e3b8f8b0a322ce713c3ac0c
Krishna Manchikalapudi <krishnam@jfrog.com>
2024-11-15 09:59:51 -0800

    jf script

--- jfcli-go.sh
+++ jfcli-go.sh
@@ -4,11 +4,21 @@ clear
 
 echo "\n\n**** JFCLI-GO.SH - BEGIN at $(date '+%Y-%m-%d-%H-%M') ****\n\n"
 
-# Config - Artifactory info
+# Config - Artifactory info 
 export JF_HOST="psazuse.jfrog.io"  JFROG_RT_USER="krishnam" JFROG_CLI_LOG_LEVEL="DEBUG" # JF_ACCESS_TOKEN="<GET_YOUR_OWN_KEY>"
-export JF_RT_URL="https://${JF_HOST}"
+export JF_RT_URL="https://${JF_HOST}" RBv2_SIGNING_KEY="krishnam"
 
-export RT_REPO_VIRTUAL="krishnam-go-virtual" BUILD_NAME="go-helloworld" BUILD_ID="cmd.$(date '+%Y-%m-%d-%H-%M')" VERSION="v$(date '+%d.%H.%M')"
+# Validate below command for the canonical version number
+export RT_REPO_VIRTUAL="krishnam-go-virtual" BUILD_NAME="go-helloworld" BUILD_ID="cmd.$(date '+%Y-%m-%d-%H-%M')" VERSION="v1.$(date '+%H').0"
+
+regex_pattern=""
+if [[ "$VERSION" =~ $regex_pattern ]]; then
+    echo "Canonical VERSION: $VERSION"
+else
+    echo "Invalid Canonical VERSION: $VERSION"
+fi
+
+# [[ "$VERSION" =~ ^(?P<major>0|[1-9]\d*)\.(?P<minor>0|[1-9]\d*)\.(?P<patch>0|[1-9]\d*)(?:-(?P<prerelease>(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+(?P<buildmetadata>[0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?$ ]] && echo "yes"
 
 echo "JF_RT_URL: $JF_RT_URL \n JFROG_RT_USER: $JFROG_RT_USER \n JFROG_CLI_LOG_LEVEL: $JFROG_CLI_LOG_LEVEL \n "
 echo "\n JF version $(jf -v) \n Go version $(go version) \n\n"
@@ -40,7 +50,9 @@ jf go build --build-name=${BUILD_NAME} --build-number=${BUILD_ID} -v
 
 # GO: Publish  # ref: https://docs.jfrog-applications.jfrog.io/jfrog-applications/jfrog-cli/cli-for-jfrog-artifactory/package-managers-integration#publishing-go-packages-to-artifactory
 echo "\n\n**** PACKAGE: Publish ****\n"
-jf go-publish v1.0.0 --build-name ${BUILD_NAME} --build-number ${BUILD_ID} --detailed-summary 
+# jf go-publish v1.0.0 --build-name ${BUILD_NAME} --build-number ${BUILD_ID} --detailed-summary 
+jf rt gp v1.0.1 --build-name=${BUILD_NAME} --build-number=${BUILD_ID} --detailed-summary 
+jf gp $BUILD_NAME $VERSION --build-name=${BUILD_NAME} --build-number=${BUILD_ID} --detailed-summary 
 
 # GO: run
 echo "\n\n**** GO: RUN ****\n"
@@ -52,9 +64,30 @@ jf rt bce ${BUILD_NAME} ${BUILD_ID}
 jf rt bag ${BUILD_NAME} ${BUILD_ID}
 jf rt bp ${BUILD_NAME} ${BUILD_ID} --detailed-summary
 
+## RBv2: release bundle - create   ref: https://docs.jfrog-applications.jfrog.io/jfrog-applications/jfrog-cli/cli-for-jfrog-artifactory/release-lifecycle-management
+echo "\n\n**** RBv2: Create ****\n\n"
+echo " BUILD_NAME: $BUILD_NAME \n BUILD_ID: $BUILD_ID \n RT_REPO_VIRTUAL: $RT_REPO_VIRTUAL  \n RBv2_SIGNING_KEY: $RBv2_SIGNING_KEY  \n "
 
+  # create spec
+export VAR_RBv2_SPEC="RBv2-SPEC-${BUILD_ID}.json"
+echo "{ \"files\": [ {\"build\": \"${BUILD_NAME}/${BUILD_ID}\", \"includeDeps\": \"false\" } ] }"  > $VAR_RBv2_SPEC
+#echo "{ \"files\": [ {\"build\": \"${BUILD_NAME}/${BUILD_ID}\", \"props\": \"build_name=${BUILD_NAME};build_id=${BUILD_ID};PACKAGE_CATEGORY=${PACKAGE_CATEGORY};state=new\" } ] }"  > RBv2-SPEC-${BUILD_ID}.json
+echo "\n" && cat $VAR_RBv2_SPEC && echo "\n"
+
+  # create RB to state=NEW
+jf rbc ${BUILD_NAME} ${BUILD_ID} --sync --access-token="${JF_ACCESS_TOKEN}" --url="${JF_RT_URL}" --signing-key="${RBv2_SIGNING_KEY}" --spec="${VAR_RBv2_SPEC}" --server-id="psazuse" # --spec-vars="build_name=${BUILD_NAME};build_id=${BUILD_ID};PACKAGE_CATEGORY=${PACKAGE_CATEGORY};state=new" 
+
+## RBv2: release bundle - DEV promote
+echo "\n\n**** RBv2: Promoted to NEW --> DEV ****\n\n"
+jf rbp --sync --access-token="${JF_ACCESS_TOKEN}" --url="${JF_RT_URL}" --signing-key="${RBv2_SIGNING_KEY}" --server-id="psazuse" ${BUILD_NAME} ${BUILD_ID} DEV  
 
 
 set +x # stop debugging from here
 echo "\n\n**** JFCLI-GO.SH - DONE at $(date '+%Y-%m-%d-%H-%M') ****\n\n"
+
+
+sleep 3
+echo "\n\n**** CLEAN UP ****\n\n"
+rm -rfv $VAR_RBv2_SPEC
+
 cd ..
